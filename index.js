f5132b0cf283ff2433e6360d2f68e6d4c6faec4b
thierrystricker-crypto <thierrystricker@gmail.com>
2025-12-06 23:50:57 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  const { code } = req.query;
-  if (!code) {
-    return res.status(400).send("Missing code");
-  }
-  const target = "https://shopify.com/apps/official-swiss-post-app/" + code;
-  return res.redirect(302, target);
-}
